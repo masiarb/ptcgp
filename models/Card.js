ae32ff109aac75a79e77d0c1c3dfd20631792607
masiar <>
2024-12-16 17:22:01 +0100

    added some todo in card schema

--- models/Card.js
+++ models/Card.js
@@ -6,13 +6,28 @@ const cardSchema = new mongoose.Schema({
   cardSet: String,
   cardNumber: Number,
   cardPack: String,
+  cardColor: {
+    type: String,
+    enum: ['Colorless', 'Fire', 'Water', 'Psychic', 'Lightning', 'Grass', 'Metal', 'Fairy', 'Darkness', 'Dragon', 'Fighting']
+  },
+  cardType: {
+    type: String,
+    enum: ['Pokémon', 'Item', 'Supporter']
+  },
   rarity: {
     type: String,
-    enum: ['Common', 'Uncommon', 'Rare', 'Double Rare', 'Illustration Rare', 'Special Illustration Rare', 'Immersive Card', 'Hyper Rare']
+    enum: ['Common', 'Uncommon', 'Rare', 'Double Rare', 'Illustration Rare', 'Special Illustration Rare', 'Immersive Card', 'Hyper Rare', 'Promo']
   },
   cardHP: Number,
-  cardDamage: Number,
+  attackName: String, //TODO: cards can have more than one attack... define schema for attacks and array of attacks here?
+  attackEffect: String,
+  attackDamage: Number, //DEFINE SCHEMA FOR COLORS?
+  pokemonAbilityName: String,
+  pokemonAbilityText: String,
+  cardText: String,
   cardImageURL: String,
+  cardIllustrator: String,
+  pokedexNumber: Number,
 }, { timestamps: true });
 
 const Card = mongoose.model('Card', cardSchema);
